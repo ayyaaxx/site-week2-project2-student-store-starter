cb02718eacfd82fa75c3d24dcf6fe219480af48e
Aya Elfettahi <aelfettahi@aelfett-ltmzept.internal.salesforce.com>
2023-06-23 16:39:42 -0700

    ...

--- student-store-express-api/app.js
+++ student-store-express-api/app.js
@@ -1,1 +1,20 @@
 // YOUR CODE HERE
+const express = require('express');
+const morgan = require('morgan'); 
+const cors = require('cors');
+const storeRoutes = require('./routes/storeRoutes');
+
+//create express applicatinon
+const app = express();
+
+//mounting middleware 
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json());
+
+// app.use('/', (req))
+
+app.use("/", storeRoutes);
+
+module.exports = app; 
+
